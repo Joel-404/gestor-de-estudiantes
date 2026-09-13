b9fd3629540f4eeef021baf51f128cabce631f68
Joel-404 <joelmina046@gmail.com>
2026-09-12 23:53:56 -0500

    se añadieron atributos y se creo el constructor

--- Estudiantes.java
+++ Estudiantes.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
+
 public class Estudiantes {
+
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String carrera;
+    private String id;
+    private ArrayList<Double> notas;
+
+
+    //constructor para 3
+    public Estudiantes (String nombre, String apellido, String correo, String carrera, String id)
+    {
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.correo=correo;
+        this.notas =new ArrayList ();
+        this.id= id;
+
+    }
     
+
+    
+
 }
